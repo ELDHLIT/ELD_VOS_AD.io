beadc938ff12ab104c72f580cf3e5e7c7b822ad1
ELDHLIT <ericldharmonic@gmail.com>
2024-08-12 12:01:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+function goToPage1Recorded() {
+  window.location.href = "Monetization-demo1-VOS360-SSAI-Brand-Insertion-recorded.html";
+}
+function goToPage1Live() {
+  window.location.href = "Monetization-demo1-VOS360-SSAI-Brand-Insertion-live.html";
+}
+function goToPage2Recorded() {
+  window.location.href = "Monetization-demo3-ad-Ecosystem-recorded.html";
+}
+function goToPage2Live() {
+  window.location.href = "Monetization-demo3-ad-Ecosystem-live.html";
+}
+function goToPage3Recorded() {
+  window.location.href = "demo2b-VOS360-ad-GAM-recorded.html";
+}
+function goToPage3Live() {
+  window.location.href = "demo2b-VOS360-ad-GAM-live.html";
+}
+function goToPage40() {
+  window.location.href = "KeyProducts-full-screen.html";
+}
+function goToPage5Recorded() {
+  window.location.href = "Live_Sport_demo1-sport-streaming-recorded.html";
+}
+function goToPage5Live() {
+  window.location.href = "Live_Sport_demo1-sport-streaming-live.html";
+}
+function goToPage6Recorded() {
+  window.location.href = "Live_Sport_demo2-sport-waas-recorded.html";
+}
+function goToPage6Live() {
+  window.location.href = "Live_Sport_demo2-sport-waas-live.html";
+}
+function goToPage7Recorded() {
+  window.location.href = "Production-and-Playout-demo2-Playout-FLOWICS-TV-recorded.html";
+}
+function goToPage7Live() {
+  window.location.href = "Production-and-Playout-demo2-Playout-FLOWICS-TV-live.html";
+}
+function goToPage8Recorded() {
+  window.location.href = "demo4b-auto-scheduling-alpha-networks-recorded.html";
+}
+function goToPage8Live() {
+  window.location.href = "demo4b-auto-scheduling-alpha-networks-live.html";
+}
+function goToPage9Recorded() {
+  window.location.href = "Monetization-demo2-Split-Screen-ad-GAM-recorded.html";
+}
+function goToPage9Live() {
+  window.location.href = "Monetization-demo2-Split-Screen-ad-GAM-live.html";
+}
+function goToPage10Recorded() {
+  window.location.href = "demo5b-broadcast-playout-SGT-TV-recorded.html";
+}
+function goToPage10Live() {
+  window.location.href = "demo5b-broadcast-playout-SGT-TV-live.html";
+}
+function goToPage11Recorded() {
+  window.location.href = "demo5c-broadcast-playout-FLOWICS-TV-recorded.html";
+}
+function goToPage11Live() {
+  window.location.href = "demo5c-broadcast-playout-FLOWICS-TV-live.html";
+}
+function goToPage12Recorded() {
+  window.location.href = "demo9-XOS-Playout-recorded.html";
+}
+function goToPage20Recorded() {
+  window.location.href = "Production-and-Playout-demo3-Spectrum-recorded.html";
+}
+function goToPage13Recorded() {
+  window.location.href = "Broadcast-and-Distribution-demo1-recorded.html";
+}
+function goToPage13Live() {
+  window.location.href = "Broadcast-and-Distribution-demo1-live.html";
+}
+function goToPage14Recorded() {
+  window.location.href = "Broadcast-and-Distribution-demo2-recorded.html";
+}
+function goToPage14Live() {
+  window.location.href = "Broadcast-and-Distribution-demo2-live.html";
+}
+function goToPage15() {
+  window.location.href = "demo10-AI-partners.html";
+}
+function goToPage16Recorded() {
+  window.location.href = "Broadcast-and-Distribution-demo3-recorded.html";
+}
+function goToPage16Live() {
+  window.location.href = "Broadcast-and-Distribution-demo3-live.html";
+}
+function goToPage17Recorded() {
+  window.location.href = "Live_Sport_demo3-Microsoft-OpenAI-recorded.html";
+}
+function goToPage17Live() {
+  window.location.href = "Live_Sport_demo3-Microsoft-OpenAI-live.html";
+}
