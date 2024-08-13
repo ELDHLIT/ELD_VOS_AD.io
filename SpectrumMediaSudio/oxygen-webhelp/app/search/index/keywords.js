6207182ea05dec2ecfe45961f28869b54926c73c
ELDHLIT <ericldharmonic@gmail.com>
2024-08-13 11:02:47 +0200

    Add files via upload

---
+++ SpectrumMediaSudio/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,24 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Settings",p:["p1"]},{w:"Contacting",p:["p2"]},{w:"Harmonic",p:["p2"]},{w:"Technical",p:["p2"]},{w:"Support",p:["p2"]},{w:"Spectrum",p:["p3","p5","p10","p11","p15"]},{w:"Media",p:["p3","p5","p10","p11","p15"]},{w:"Studio",p:["p3","p5","p10","p11","p15"]},{w:"Installation",p:["p3"]},{w:"Introduction",p:["p4","p5"]},{w:"to",p:["p5"]},{w:"the",p:["p5"]},{w:"Main",p:["p6"]},{w:"Features",p:["p6"]},{w:"Ordering",p:["p7"]},{w:"Guide",p:["p7"]},{w:"Overview",p:["p8"]},{w:"Panels",p:["p9"]},{w:"Configuration",p:["p10"]},{w:"User",p:["p11"]},{w:"Interface",p:["p11"]},{w:"System",p:["p12"]},{w:"Environment",p:["p12"]},{w:"Installing",p:["p13","p14","p15"]},{w:"FIT",p:["p13"]},{w:"Service",p:["p13","p14"]},{w:"Router",p:["p14"]},{w:"Copyright",p:["p16"]},{w:"notices",p:["p16"]},{w:"Documentation",p:["p17"]},{w:"conventions",p:["p17"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4, 5, 6, 7];
+ph["p3"]=[8, 9, 10, 11];
+ph["p4"]=[12];
+ph["p5"]=[12, 13, 14, 8, 9, 10];
+ph["p6"]=[15, 16];
+ph["p7"]=[17, 18];
+ph["p8"]=[19];
+ph["p9"]=[20];
+ph["p10"]=[8, 9, 10, 21];
+ph["p12"]=[24, 25];
+ph["p11"]=[8, 9, 10, 22, 23];
+ph["p14"]=[26, 29, 28];
+ph["p13"]=[26, 27, 28];
+ph["p16"]=[30, 31];
+ph["p15"]=[26, 8, 9, 10];
+ph["p17"]=[32, 33];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ SpectrumMediaSudio/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {7:-1,1:10,14:3,9:10,2:-1,5:-1,15:3,13:3,6:5,12:5,4:3,3:-1,11:-1,10:-1,8:5};
